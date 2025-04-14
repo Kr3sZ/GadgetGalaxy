a9476ec5ccc0489ef4e276bfcea9efeb57db2414
Krisz B <20959234-kr1sz@users.noreply.gitlab.com>
2025-04-14 19:33:55 +0200

    redirection

--- FRONTEND/src/app/app.routes.ts
+++ FRONTEND/src/app/app.routes.ts
@@ -5,11 +5,17 @@ import { ProfileComponent } from './profile/profile.component';
 import { CartComponent } from './cart/cart.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import {AdminComponent} from './admin/admin.component';
+import {AdminLoginComponent} from './admin-login/admin-login.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'cart', component: CartComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'admin/login', component: AdminLoginComponent },
+  { path: '**', component: NotFoundComponent}
 ];
